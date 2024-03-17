b5a4686c1689d676f7edde5e8e8cf36134716405
jcorderob <jcorderob@gmail.com>
2024-03-17 18:54:05 -0300

    Add files via upload

---
+++ 02_secuencias_escape.py
@@ -0,0 +1,16 @@
+
+
+# --------------------------------  EJERCICIO 2 ------------------------------
+# ---------------------------   SECUENCIAS DE ESCAPE -------------------------
+
+"""
+  \n   ->  salto de linea
+  \t   ->  tabulación
+  \cualquierCaracter  -> \'    \"   
+"""
+
+print ("hola \" mundo")
+print ("hola \t mundo")
+print ("hola \nmundo")
+
+
