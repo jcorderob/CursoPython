1f7b7ebdfd68788ffa366c1a19401dd49ae6676e
jcorderob <jcorderob@gmail.com>
2024-03-17 18:51:37 -0300

    Add files via upload

---
+++ 01_salida_basica.py
@@ -0,0 +1,31 @@
+
+import os
+
+# Este es un comentario de una sola linea
+
+"""
+    Este es un comentario multilinea 
+    agaggagagagagga
+    jdjsjsjsjsjsjsjsj
+    sjsjsjsjsjsjsjsj 
+"""
+
+""" --------------------------------------------------------
+    Programa que realiza las 4 operaciones aritméticas básicas
+    a saber, suma , resta, multiplicación y división. Los datos
+    de partida se generan internamente.
+    ---------------------------------------------------------
+""" 
+
+
+
+# --------------------------------  EJERCICIO 1 ------------------------------
+# Programa para generar un nombre una edad y mostrarlos en salida por pantalla
+
+os.system("cls")
+nombre = "Alicia Medina"
+edad = 35
+print("Me llamo", nombre, 'y tengo', edad, "años.")
+
+
+
