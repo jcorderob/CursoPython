eddf31912efc1df131b7380fb2d99ce5b485b457
jcorderob <jcorderob@gmail.com>
2024-04-27 17:52:59 -0400

    Add files via upload

---
+++ 05_graficos_con_grid.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+
+os.system("cls")   
+ypuntos = np.array([3,500,100,300,50]) 
+ypuntos2 = np.array([10,100,450,50,500]) 
+
+# -------- Cambiando la fuente de las etiquetas  -----------
+diccFuenteTitulo = {'family':'serif','color':'blue','size':20}
+diccFuenteEjes   = {'family':'serif','color':'darkred','size':15}
+
+# --------------  Dibujando las etiquetas  -----------------
+plt.plot(ypuntos)
+plt.plot(ypuntos2)
+plt.title("TITULO DEL GRÁFICO", fontdict= diccFuenteTitulo)
+plt.xlabel("Leyenda eje X", fontdict= diccFuenteEjes)
+plt.ylabel("Leyenda eje Y", fontdict= diccFuenteEjes)
+
+# ------------  Dibujando la rejilla o cuadrícula  --------------
+plt.grid(color = 'green', linestyle = '--', linewidth = 0.5)
+plt.show()
+
