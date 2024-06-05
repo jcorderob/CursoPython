dab7ec7abf15d1416a323e9fbd2ba28a86c113fa
jcorderob <jcorderob@gmail.com>
2024-06-05 14:47:50 -0400

    Add files via upload

---
+++ 05_herencia.py
@@ -0,0 +1,8 @@
+import os
+from paquete_clases.clases import Persona, Profesional, \
+     Estudiante, Deportista, Futbolista
+from paquete_implementacion.implementacion import *
+
+if __name__ == "__main__":
+    implementacion()
+ 
